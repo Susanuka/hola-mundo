109cca231da644e6cb22ccb04df112b4783698ff
susanuka <susanuka@gmail.com>
2024-06-10 12:07:20 +0200

    chor: agregado componente header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { FC, PropsWithChildren } from 'react'
 
+import Header from './Header/Header'
+
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
-      <header>Cabecera</header>
+      <Header title="mi titulo" />
       <main>{children}</main>
       <footer>Pie de página</footer>
     </div>
