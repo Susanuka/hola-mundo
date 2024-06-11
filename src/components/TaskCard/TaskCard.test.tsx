ebde2b2cc5a2b7d363a6aba7d303a3e71b61f48d
susanuka <susanuka@gmail.com>
2024-06-11 12:27:33 +0200

    feat: test de la TaskCard

--- src/components/TaskCard/TaskCard.test.tsx
+++ src/components/TaskCard/TaskCard.test.tsx
@@ -1,14 +1,60 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent, { UserEvent } from '@testing-library/user-event'
+import { TasksContext } from '../TasksContext/TasksContext'
 import TaskCard from './TaskCard'
 
 describe('TaskCard', () => {
-  it('renderiza el componente', () => {
-    render(<TaskCard task={{ id: 1, title: 'Task 1', description: 'Descripcion', completed: true }}></TaskCard>)
+  let user: UserEvent
+  beforeEach(() => {
+    user = userEvent.setup()
   })
-  it('renderiza el titulo de la tarea', () => {
+  afterEach(() => {})
+  beforeAll(() => {})
+  afterAll(() => {})
+
+  test('renderiza el componente', () => {
+    render(<TaskCard task={{ id: 1, title: 'Task 1', description: '', completed: false }} />)
+  })
+
+  it('muestra el título de la tarea', () => {
+    render(<TaskCard task={{ id: 1, title: 'Task 1', description: '', completed: false }} />)
+    expect(screen.getByText('Task 1')).toBeInTheDocument()
+  })
+
+  it('muestra la descripción de la tarea', () => {
     const { getByText } = render(
-      <TaskCard task={{ id: 1, title: 'Task 1', description: 'Descripcion', completed: true }}></TaskCard>,
+      <TaskCard task={{ id: 1, title: 'Task 1', description: 'Description', completed: false }} />,
     )
-    expect(getByText('Task 1')).toBeInTheDocument()
+    expect(getByText('Description')).toBeInTheDocument()
+  })
+
+  it('muestra la tarea como completada', () => {
+    const { getByRole } = render(
+      <TaskCard task={{ id: 1, title: 'Task 1', description: 'Description', completed: true }} />,
+    )
+    expect(getByRole('checkbox')).toBeChecked()
+  })
+
+  it('muestra la tarea como no completada', () => {
+    const { getByRole } = render(
+      <TaskCard task={{ id: 1, title: 'Task 1', description: 'Description', completed: false }} />,
+    )
+    expect(getByRole('checkbox')).not.toBeChecked()
+  })
+
+  it('marca la tarea como completada', async () => {
+    // 1 - preparo el contexto del test
+    const task = { id: 1, title: 'Task 1', description: 'Description', completed: false }
+    const updateSpy = vitest.fn()
+    const { getByRole } = render(
+      <TasksContext.Provider value={{ tasks: [], addTask: () => {}, updateTask: updateSpy }}>
+        <TaskCard task={task} />,
+      </TasksContext.Provider>,
+    )
+    // 2- Actúo
+    await user.click(getByRole('checkbox'))
+
+    // 3- compruebo resultado
+    expect(updateSpy).toHaveBeenCalledWith({ ...task, completed: !task.completed })
   })
 })
